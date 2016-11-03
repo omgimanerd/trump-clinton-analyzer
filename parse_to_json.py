00031ea66494726e5dc10c5c09e7c763f436ccd3
omgimanerd <alvin.lin.dev@gmail.com>
2016-11-03 02:16:20 -0400

    selected all trump clinton sentences

--- parse_to_json.py
+++ parse_to_json.py
@@ -9,7 +9,6 @@ import re
 def has_speaker(sentence):
     try:
         name = sentence.split(' ')[0]
-        print(name, name[:-1] if name.isupper() and ':' in name else None)
         return name[:-1] if name.isupper() and ':' in name else None
     except:
         return None
@@ -20,18 +19,31 @@ def parse(filename):
     data = re.sub('\[.+\]', '', data)
     data = re.sub('\s{2,}', '\n', data)
     data = data.split('\n')
-    speakers = defaultdict(str)
+    speakers = defaultdict(list)
     current_speaker = None
     for sentence in data:
         speaker = has_speaker(sentence)
         if speaker is not None:
             current_speaker = speaker
             sentence = sentence[len(speaker) + 2:]
-        speakers[current_speaker] += sentence
+        speakers[current_speaker].append(sentence)
     return speakers
 
+def get_trump_clinton(files):
+    trump = []
+    clinton = []
+    for f in files:
+        data = parse(f)
+        trump += data['TRUMP']
+        clinton += data['CLINTON']
+    return {
+        'TRUMP': trump,
+        'CLINTON': clinton
+    }
 
 if __name__ == '__main__':
-    files = os.listdir('data')
-    data = parse(os.path.join('data', files[0]))
-    print(data.keys())
+    data = get_trump_clinton([os.path.join('data', f) for f in os.listdir('data')])
+    with open('trump.json', 'w') as trump:
+        json.dump(data['TRUMP'], trump)
+    with open('clinton.json', 'w') as clinton:
+        json.dump(data['CLINTON'], clinton)
